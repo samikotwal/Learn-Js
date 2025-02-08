9bb863613a07165555b19996d6b5d13e37648f89
samikotwal <samikotwal15@gmail.comVv>
2025-02-08 17:33:00 +0530

    DataType

---
+++ 01_basic/01_DataType.js
@@ -0,0 +1,15 @@
+//number => 2 to 54
+//bigint
+//String => "Sami"
+//Boolean => True/False
+//null => StandAlone value 
+//undefined => undefined 
+
+
+let name = "sami"
+let age = 2
+let boolean = true
+let satate;
+
+//console.table([typeof name, age, boolean, satate])
+console.log(typeof age)
---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accountEmail = "samikotwal@gmail.com"
+var accountpassword = "123"
+accountcity ="jaipur"
+let accountSatate ;
+
+
+
+accountEmail = "s@g.com"
+accountpassword = "2121"
+accountcity="benguluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountcity, accountSatate])
