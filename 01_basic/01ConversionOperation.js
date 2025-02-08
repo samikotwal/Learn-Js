4e6449f1ba6edd14d81e4cd362cf9754f444a26a
samikotwal <samikotwal15@gmail.comVv>
2025-02-08 19:43:28 +0530

    Conversion Opteration

---
+++ 01_basic/01ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = "sami"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NanN
+//true => 1; false => 0
+
+let isLoggedIn = "33"
+
+let booleanIn = Number(isLoggedIn)
+console.log( isLoggedIn);
+
+//1 => true; 0 => false
+//"" =>false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof someNumber);
+console.log(someNumber);
+
