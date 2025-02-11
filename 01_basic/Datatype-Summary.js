3dc0cc31ed0a826714e8474317e0295494150a41
samikotwal <samikotwal15@gmail.comVv>
2025-02-11 22:46:57 +0530

    Datatype-summary

---
+++ 01_basic/Datatype-Summary.js
@@ -0,0 +1,30 @@
+//primitive
+
+//7 types : String, Number, Boolean,null, undefined, symbol,BigInt
+
+const score = 100
+const scoreValue=100
+
+const Logginedin = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('1234')
+console.log(id === anotherId);
+
+// Non-primitive type 
+// Array , Objects, Function
+
+const sam  = ["smi", "ami", "ams"];
+let myobj = {
+    name:"sami",
+    age :23
+}
+
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof anotherId);
+
